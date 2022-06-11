0136e198e87db1efb44424ed1387ef0bc593f24f
kevinjosue2326 <kjcm_dark@outlook.com>
2022-06-10 23:05:29 -0600

    Starting Development Plugin for Woocomerce

---
+++ wp-chivowallet/wp-chivowallet.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: WP Chivo Wallet
+ * Plugin URI: https://github.com/kevinjosue2326/wp-chivowallet
+ * Description: Accept Payments with Chivo Wallet quickly and easily
+ * Author: Kevin Cruz
+ * Author URI: https://github.com/kevinjosue2326
+ * Version: 0.0.1
+ */
